be294788f5d95e51286317e062226f336323f3ed
Madipoupou <poupoumadi@gmail.com>
2020-07-17 14:33:22 +0200

    Comments on dices.py project

--- dices.py
+++ dices.py
@@ -46,9 +46,11 @@ class FaceDom(object):
         self.can.delete(self.dice)
     
     def lock(self):
+        # Change the dice color when it is locked
         self.can.itemconfig(self.dice, fill='grey')
     
     def unlock(self):
+        # Change the dice color when it is unlocked
         self.can.itemconfig(self.dice, fill='ivory')
 
 class Projet(tk.Frame):
@@ -74,9 +76,12 @@ class Projet(tk.Frame):
     
     def boutA(self):
         """ Create six dices each one having a random value """
+        # Saving the old dices and cleaning the list of dices, removing them from canvas
         temp = self.dices
         if (self.dices != []):
             self.boutB()
+        
+        # Creating the new dice shapes with random values
         dices_shapes = [
             [randint(1, 6), (100, 200), 100],
             [randint(1, 6), (300, 200), 100],
@@ -85,9 +90,13 @@ class Projet(tk.Frame):
             [randint(1, 6), (300, 400), 100],
             [randint(1, 6), (500, 400), 100]
         ]
+
+        # Retrieving the locked dices values to keep them
         for i in range(len(temp)):
             if (temp[i]['locked']):
                 dices_shapes[i][0] = temp[i]['val']
+        
+        # Creating the dice using the shapes defined earlier
         for i in range(len(dices_shapes)):
             dice = dices_shapes[i]
             self.dices.append({
@@ -97,6 +106,7 @@ class Projet(tk.Frame):
                 'val': dice[0],
                 'locked': False
             })
+            # If the dice was locked before the new dice generation, we keep it locked
             if (temp != [] and temp[i]['locked']):
                 self.dices[i]['locked'] = True
                 self.dices[i]['dice'].lock()
@@ -112,11 +122,14 @@ class Projet(tk.Frame):
         self.master.destroy()
     
     def on_click(self, event):
+        """ Manage the click on the dices to lock/unlock them """
         x = event.x
         y = event.y
         for dice in self.dices:
+            # Saving the current dice position (interval)
             xmin, xmax = dice['coord'][0] - dice['size'] / 2, dice['coord'][0] + dice['size'] / 2
             ymin, ymax = dice['coord'][1] - dice['size'] / 2, dice['coord'][1] + dice['size'] / 2
+            # Checking if the click was on a dice and locking/unlocking it
             if (x >= xmin and x <= xmax and y >= ymin and y <= ymax):
                 if (dice['locked']):
                     dice['locked'] = False
